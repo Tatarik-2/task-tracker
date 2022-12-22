931df455f4282b3d5d7828e01677e60da95759ec
astokarev <sandrotokarev@gmail.com>
2022-12-22 15:06:59 +0300

    добавил валидацию, перенес маппинг Entity>dto в сервис слой, другие мелкие замечания

--- src/main/java/ewp/tasktracker/api/dto/LabelsDto.java
+++ src/main/java/ewp/tasktracker/api/dto/LabelsDto.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,8 +14,6 @@ public class LabelsDto {
     private String author_id;
 
     private String task_id;
-    private Date created_at;
-    private Date updated_at;
 
     public LabelsDto(LabelsEntity labelsEntity) {
         this.id = labelsEntity.getId();
