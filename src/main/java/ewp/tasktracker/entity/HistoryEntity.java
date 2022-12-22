d11e82f35947e4f6b6ad91fc232613e6db820191
AlKichen <kich7777777@gmail.com>
2022-12-22 18:15:15 +0300

    Реализация функционала создания историй и получения истории по id - добавил реализацию createdAt и updatedAt через аннотации

--- src/main/java/ewp/tasktracker/entity/HistoryEntity.java
+++ src/main/java/ewp/tasktracker/entity/HistoryEntity.java
@@ -6,7 +6,10 @@ import ewp.tasktracker.service.StatusEnum2;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
@@ -30,7 +33,11 @@ public class HistoryEntity extends BaseEntity {
 
     private String sprintId;
     // в будущем вынести в BaseEntity
+    @CreationTimestamp
+    @Column(name = "CREATED_AT")
     private LocalDateTime createdAt;
+    @UpdateTimestamp
+    @Column(name = "UPDATED_AT")
     private LocalDateTime updatedAt;
 
     public HistoryEntity(String name, String description, StatusEnum2 status, PriorityEnum priority,
@@ -42,8 +49,5 @@ public class HistoryEntity extends BaseEntity {
         this.epicId = epicId;
         this.authorId = authorId;
         this.sprintId = sprintId;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-
     }
 }
