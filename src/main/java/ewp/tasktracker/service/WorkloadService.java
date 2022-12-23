c4e9d76d18990a9d29c84194d7c09a7f4775f4a6
Tatarik_2 <WPt-VRn-max-UCv>
2022-12-23 21:01:02 +0300

    Реализация функционала создания рабочего пространства. v.4.0

--- src/main/java/ewp/tasktracker/service/WorkloadService.java
+++ src/main/java/ewp/tasktracker/service/WorkloadService.java
@@ -2,7 +2,6 @@ package ewp.tasktracker.service;
 
 import ewp.tasktracker.api.dto.CreateWorkloadRq;
 import ewp.tasktracker.api.dto.WorkloadDto;
-import ewp.tasktracker.entity.WorkloadEntity;
 
 import java.util.List;
 
