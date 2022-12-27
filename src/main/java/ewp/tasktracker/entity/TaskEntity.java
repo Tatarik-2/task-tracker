6661f82d101dce638619f39da237cda2705116d1
Роман <mackarow.romik@yandex.ru>
2022-12-27 19:19:51 +0300

    Реализация функционала обновления задач.

--- src/main/java/ewp/tasktracker/entity/TaskEntity.java
+++ src/main/java/ewp/tasktracker/entity/TaskEntity.java
@@ -1,17 +1,20 @@
 package ewp.tasktracker.entity;
 
+import ewp.tasktracker.entity.common.BaseEntity;
 import ewp.tasktracker.entity.common.Priority;
 import ewp.tasktracker.entity.common.ProgressStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "tasks")
