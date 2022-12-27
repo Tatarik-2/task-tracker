590a4f0453b19e595d6c626c237513436d4adc3e
prodglockem <prodglockem@gail.com>
2022-12-27 19:56:34 +0300

    Реализация функционала удаления комментариев

--- src/main/java/ewp/tasktracker/api/controller/CommentController.java
+++ src/main/java/ewp/tasktracker/api/controller/CommentController.java
@@ -9,7 +9,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +30,7 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    @ApiOperation(value = "Сохранить", response = CommentDto.class)
+    @ApiOperation(value = "Сохранить комментарий", response = CommentDto.class)
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "Успешное создание комментария"),
             @ApiResponse(code = 500, message = "Внутрення ошибка сервера"),
@@ -43,7 +42,7 @@ public class CommentController {
     }
 
     @PatchMapping
-    @ApiOperation(value = "Сохранить", response = CommentDto.class)
+    @ApiOperation(value = "Обновить комментарий", response = CommentDto.class)
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "Успешное обновление комментария"),
             @ApiResponse(code = 500, message = "Внутрення ошибка сервера"),
@@ -52,6 +51,18 @@ public class CommentController {
     })
     public ResponseEntity<CommentDto> updateComment(@Valid @RequestBody UpdateCommentRq request) {
         CommentDto response = commentService.update(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Удалить комментарий", response = CommentDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Успешное удаление комментария"),
+            @ApiResponse(code = 500, message = "Внутрення ошибка сервера"),
+            @ApiResponse(code = 404, message = "Комментарий не найден")
+    })
+    public ResponseEntity<CommentDto> deleteComment(@PathVariable String id) {
+        CommentDto response = commentService.deleteById(id);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
