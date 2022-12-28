3ecbf3ffdd86ca87510b737c89d37659f9ecb98e
Tatarik_2 <WPt-VRn-max-UCv>
2022-12-29 00:09:25 +0300

    Реализация функционала создания, редактирования, поиска по id и вызова списка суперспринтов, v.2.0 Внесены изменения по замечаниям

--- src/main/java/ewp/tasktracker/entity/SupersprintEntity.java
+++ src/main/java/ewp/tasktracker/entity/SupersprintEntity.java
@@ -4,6 +4,8 @@ import ewp.tasktracker.entity.common.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Entity;
 import java.time.LocalDateTime;
@@ -18,7 +20,22 @@ public class SupersprintEntity extends BaseEntity{
     private LocalDateTime startAt;
     private LocalDateTime endAt;
     private String authorId;
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
+    public SupersprintEntity(String name, LocalDateTime startAt, LocalDateTime endAt, String authorId) {
+        this.name = name;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.authorId = authorId;
+    }
+    public SupersprintEntity(String name, LocalDateTime startAt, LocalDateTime endAt, String authorId, LocalDateTime createdAt) {
+        this.name = name;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.authorId = authorId;
+        this.createdAt = createdAt;
+    }
 }
