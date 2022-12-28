a4bc1f49ae62e78fb1786ba0ec5abdf5f1c52261
AlKichen <kich7777777@gmail.com>
2022-12-28 15:52:55 +0300

    Реализация функционала поиска истории

--- src/main/resources/scripts/release_0_0_2/scripts/create_index/create_index_for_table_histories.sql
+++ src/main/resources/scripts/release_0_0_2/scripts/create_index/create_index_for_table_histories.sql
@@ -2,7 +2,7 @@
 
 --changeset kichenko-av:create_index_for_table_histories rollbackSplitStatements:true
 --comment: Создание индекса для таблицы историй
-CREATE UNIQUE INDEX histories_name_idx
+CREATE INDEX histories_name_idx
 ON histories(name);
 
 
