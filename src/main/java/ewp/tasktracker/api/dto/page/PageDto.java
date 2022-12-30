021bb2a71fb6d4ca702f470b1d1317964d0ca5e1
AlKichen <kich7777777@gmail.com>
2022-12-30 21:02:29 +0300

    Реализация функционала поиска истории - вынес метод в PageDto

--- src/main/java/ewp/tasktracker/api/dto/page/PageDto.java
+++ src/main/java/ewp/tasktracker/api/dto/page/PageDto.java
@@ -1,15 +1,24 @@
 package ewp.tasktracker.api.dto.page;
 
+import ewp.tasktracker.api.dto.history.HistoryDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PageDto<T> {
     private List<T> items;
     private Integer pageNumber;
     private Integer pageSize;
     private Integer total;
+
+    public static <T> PageDto<T> getEmptyPageDto(){
+        PageDto<T> pageDto = new PageDto<>();
+        pageDto.setTotal(0);
+        return pageDto;
+    }
 }
