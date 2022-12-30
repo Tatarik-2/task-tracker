97277cafe619d6d470318698b780479e517976c5
BFVPR <sitisaw713@gmail.com>
2022-12-30 23:52:38 +0500

    Вынес добавление индекса в отдельный скрипт Поправил методы в сервисе

--- src/main/java/ewp/tasktracker/repository/EpicRepository.java
+++ src/main/java/ewp/tasktracker/repository/EpicRepository.java
@@ -2,10 +2,7 @@ package ewp.tasktracker.repository;
 
 
 import ewp.tasktracker.entity.EpicEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,7 +10,6 @@ import java.util.List;
 
 public interface EpicRepository extends JpaRepository<EpicEntity, String> {
 
-//    @Query(value = "select e from epics e WHERE e.name = ?1")
-//    Page<EpicEntity> findEpicEntityByName(String name, Pageable pageable);
+    List<EpicEntity> findByName(String name);
 
 }
