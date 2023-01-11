20ed23847e9bed20494134aeac821317a86e1584
Alexey Kichenko <kich126@mail.ru>
2023-01-11 19:59:46 +0000

    HistoryRepository - bugfix (метод findByName - добавлена пагинация при запросе в БД)

--- src/main/java/ewp/tasktracker/service/history/HistoryServiceImpl.java
+++ src/main/java/ewp/tasktracker/service/history/HistoryServiceImpl.java
@@ -9,7 +9,11 @@ import ewp.tasktracker.entity.HistoryEntity;
 import ewp.tasktracker.exception.ResourceNotFoundException;
 import ewp.tasktracker.repository.HistoryRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -52,12 +56,11 @@ public class HistoryServiceImpl implements HistoryService {
 
     @Override
     public PageDto<HistoryDto> findHistoryByName(String name, Integer pageSize, Integer pageNumber) {
-        List<HistoryEntity> historyEntityList = historyRepository.findByName(name);
-        if (historyEntityList.isEmpty()) {
-            return PageDto.getEmptyPageDto();
-        }
         pageSize = pageUtil.pageSizeControl(pageSize);
-        List<HistoryDto> historyDtoList = historyEntityList.stream().map(HistoryDto::new).collect(Collectors.toList());
+        Page<HistoryEntity> historyEntityPage = historyRepository.findByName(name.toUpperCase(),
+                Pageable.ofSize(pageSize).withPage(pageNumber));
+        List<HistoryDto> historyDtoList = historyEntityPage.getContent()
+                .stream().map(HistoryDto::new).collect(Collectors.toList());
         return new PageDto<>(historyDtoList, pageNumber, pageSize, historyDtoList.size());
     }
 }
