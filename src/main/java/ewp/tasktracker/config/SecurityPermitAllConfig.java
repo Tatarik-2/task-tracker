596bf076202eace70de76fcaac56a51e3558ef05
AlKichen <kich7777777@gmail.com>
2023-01-13 11:20:54 +0300

    переделал SecurityConfig без deprecated класса - проверил все работает

--- src/main/java/ewp/tasktracker/config/SecurityPermitAllConfig.java
+++ src/main/java/ewp/tasktracker/config/SecurityPermitAllConfig.java
@@ -1,14 +1,28 @@
 package ewp.tasktracker.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class SecurityPermitAllConfig  {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll()
                 .and().csrf().disable();
+        return http.build();
     }
+
+    /*protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+    }*/
 }
