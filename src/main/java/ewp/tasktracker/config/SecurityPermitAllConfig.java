5c198cfa7a0e2a4aa43c59cfc10e1716610ebec6
AlKichen <kich7777777@gmail.com>
2023-01-13 11:22:06 +0300

    удалил закомментированный код и не используемые ипморты

--- src/main/java/ewp/tasktracker/config/SecurityPermitAllConfig.java
+++ src/main/java/ewp/tasktracker/config/SecurityPermitAllConfig.java
@@ -3,16 +3,10 @@ package ewp.tasktracker.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityPermitAllConfig  {
+public class SecurityPermitAllConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,9 +14,4 @@ public class SecurityPermitAllConfig  {
                 .and().csrf().disable();
         return http.build();
     }
-
-    /*protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll()
-                .and().csrf().disable();
-    }*/
 }
