cefeeb833c106d7d8372f3a4310188b09113baa9
BFVPR <sitisaw713@gmail.com>
2023-01-20 16:40:19 +0500

    Добавил тесты к эпикам Фикс EpicRepository

--- src/main/java/ewp/tasktracker/repository/EpicRepository.java
+++ src/main/java/ewp/tasktracker/repository/EpicRepository.java
@@ -1,18 +1,15 @@
 package ewp.tasktracker.repository;
 
 import ewp.tasktracker.entity.EpicEntity;
-import ewp.tasktracker.entity.HistoryEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface EpicRepository extends JpaRepository<EpicEntity, String> {
 
     @Query("SELECT ep FROM epics ep WHERE UPPER(ep.name) LIKE %?1%")
     Page<EpicEntity> findByName(String filterForName, Pageable pageable);
-    List<EpicEntity> findByName(String name);
+
 
 }
